571416b2a841e62b2957ee5f60056c2714897abb
Eunice <eunicepatrina0@gmail.com>
2023-08-04 14:26:57 +0530

    optimize the main.py by instructor code

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import pandas
 
+st.set_page_config(layout="wide")
 
-st.title("The Best Company")
+st.header("The Best Company")
+
 
 content = """ A company, abbreviated as co., is a legal entity representing an association of people, 
 whether natural, legal or a mixture of both, with a specific objective. Company members share a common purpose 
 and unite to achieve specific, declared goals. """
 st.write(content)
 
-st.title("Our Team")
+st.subheader("Our Team")
 
 data = pandas.read_csv("data.csv")
 
@@ -19,22 +21,23 @@ col1, col2, col3 = st.columns(3)
 
 
 with col1:
+    output_format(ata[:length1])
     for index, value in data[:length1].iterrows():
         name = "{} {}".format(value["first name"], value["last name"])
-        st.title(name.title())
+        st.subheader(name.title())
         st.write(value["role"])
         st.image(f"images/{value['image']}")
 
 with col2:
     for index, value in data[length1:length2].iterrows():
         name = "{} {}".format(value["first name"], value["last name"])
-        st.title(name.title())
+        st.subheader(name.title())
         st.write(value["role"])
         st.image(f"images/{value['image']}")
 
 with col3:
     for index, value in data[length2:].iterrows():
         name = "{} {}".format(value["first name"], value["last name"])
-        st.title(name.title())
+        st.subheader(name.title())
         st.write(value["role"])
         st.image(f"images/{value['image']}")
