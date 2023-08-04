0350e2dc3bc863c1e94e681a2eff16b6a08d3bdb
Eunice <eunicepatrina0@gmail.com>
2023-08-04 19:52:28 +0530

    sent email

---
+++ send_email.py
@@ -0,0 +1,15 @@
+import smtplib, ssl
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    email = "eunicepatrina0@gmail.com"
+    password = "vnsmyhpurcjzbzld"
+
+    context = ssl.create_default_context()
+
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(email, password)
+        server.sendmail(email, email, message)
